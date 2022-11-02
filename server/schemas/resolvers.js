2502fc09c271e7acfa22da0cc8842479a00706a5
justin <fezzygo@gmail.com>
2022-11-01 22:14:26 -0400

    updated the graphql schema to add Profiles and Posts (resolvers.js:6-37, typeDefs.js:6-8,11-15,21,22,25-29)

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,40 @@
-const { model } = require("mongoose");
-const { Profile } = require("../models");
+const { Profile, Post } = require("../models");
 
 const resolvers = {
     Query: {
         profiles: async () => {
-            return Profile.find();
+            return Profile.find().populate('posts');
+        },
+        profile: async (parent, { profileId }) => {
+            return Profile.findOne({ id_: profileId }).populate('posts');
+        },
+        posts: async () => {
+            return Post.find({});
+        },
+        post: async (parent, { postId }) => {
+            return Post.findOne({ id_: postId });
+        },
+    },
+
+    Mutation: {
+        addProfile: async (parent, { firstName, lastName, posts }) => {
+            return await Profile.create({ firstName, lastName, posts });
+        },
+        addPost: async (parent, { profileId, title, description }) => {
+            const post = await Post.create({
+                profileId,
+                title,
+                description,
+            });
+            await Profile.findOneAndUpdate(
+                { _id: profileId },
+                {
+                    $addToSet: {
+                        posts: post._id,
+                    },
+                }
+            );
+            return post;
         },
     },
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,15 +1,31 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-  type Profile {
-    _id: ID
-    name: String
-    skills: [String]!
-  }
+    type Profile {
+        _id: ID
+        firstName: String
+        lastName: String
+        posts: [Post]
+    }
 
-type Query {
-    profiles: [Profile]!
-    profile(profileId: ID!): Profile
-  }
-`
+    type Post {
+        _id: ID
+        profileId: String
+        title: String
+        description: String
+    }
+
+    type Query {
+        profiles: [Profile]
+        profile(profileId: ID): Profile
+        posts: [Post]
+        post(_id: ID!): Post
+    }
+
+    type Mutation {
+        addProfile(firstName: String, lastName: String): Profile
+        addPost(profileId:ID, title: String, description: String): Post
+    }
+`;
+
 module.exports = typeDefs;
