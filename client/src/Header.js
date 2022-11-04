8628a3a4331777fbd5b268000152a2d05e147ad1
justin <fezzygo@gmail.com>
2022-11-03 21:25:03 -0400

    Added header and, logo, and  favicon

---
+++ client/src/Header.js
@@ -0,0 +1,213 @@
+import React from "react";
+
+export default function Header () {
+    return (
+        <header>
+        {/* logo */}
+        <svg id="shape3" width="100%" height="1474.56pt">
+            <path
+                transform="matrix(0.591049131505232 0 0 0.591049131505232 116.703631810283 37.8825155920013)"
+                fill="none"
+                stroke="#01f1ed"
+                stroke-width="7.2"
+                stroke-linecap="square"
+                stroke-linejoin="bevel"
+                d="M14.6627 58.1804C14.6627 38.4236 -10.3676 18.1436 4.97848 2.79757C6.09352 1.68253 28.7059 -0.83503 29.8185 0.277571C33.5387 0.397826 55.7131 -1.54785 63.6584 6.39753C70.2558 12.9949 74.5548 21.4503 76.3994 25.3314C77.5598 27.8918 86.5349 51.3542 86.5349 53.0032"
+            />
+            <path
+                transform="matrix(0.591049131505232 0 0 0.591049131505232 70.9200436500669 67.0891364813602)"
+                fill="#000000"
+                fill-rule="evenodd"
+                stroke="#ffffff"
+                stroke-width="3.6"
+                stroke-linecap="square"
+                stroke-linejoin="bevel"
+                d="M15.12 11.7027L0 257.223L0 257.223L161.325 302.043C168.017 204.183 169.653 102.944 169.74 0.00268739C171.3 -0.117331 119.76 3.78266 15.12 11.7027Z"
+            />
+            <text
+                transform="matrix(0.621000971406233 0.066489990391713 -0.0789248139191006 0.78838457738806 95.743154607456 114.261934847784)"
+                fill="#fd004f"
+                stroke-opacity="0"
+                stroke="#000000"
+                stroke-width="0"
+                stroke-linecap="square"
+                stroke-linejoin="bevel"
+                font-family="Noto Sans"
+                font-size="28"
+                font-size-adjust="0.372962"
+                font-stretch="normal"
+                font-weight="700"
+                letter-spacing="0"
+                text-anchor="middle"
+                word-spacing="0"
+            >
+                <tspan x="47.671875">Tiktok </tspan>
+                <tspan x="47.671875" dy="38.25">
+                    made{" "}
+                </tspan>
+                <tspan x="47.671875" dy="38.25">
+                    me{" "}
+                </tspan>
+                <tspan x="47.671875" dy="38.25">
+                    buy it
+                </tspan>
+            </text>
+            <text
+                transform="matrix(0.621000957986502 0.0664899889548751 -0.0789248122135481 0.788384560351196 94.2628515388136 113.218217254555)"
+                fill="#02f0eb"
+                stroke-opacity="0"
+                stroke="#000000"
+                stroke-width="0"
+                stroke-linecap="square"
+                stroke-linejoin="bevel"
+                font-family="Noto Sans"
+                font-size="28"
+                font-size-adjust="0.372962"
+                font-stretch="normal"
+                font-weight="700"
+                letter-spacing="0"
+                text-anchor="middle"
+                word-spacing="0"
+            >
+                <tspan x="47.671875">Tiktok </tspan>
+                <tspan x="47.671875" dy="38.25">
+                    made{" "}
+                </tspan>
+                <tspan x="47.671875" dy="38.25">
+                    me{" "}
+                </tspan>
+                <tspan x="47.671875" dy="38.25">
+                    buy it
+                </tspan>
+            </text>
+            <text
+                transform="matrix(0.621000970097047 0.0664899902515397 -0.0789248137527124 0.788384575725998 94.8113695772461 113.955518032661)"
+                fill="#ffffff"
+                stroke-opacity="0"
+                stroke="#000000"
+                stroke-width="0"
+                stroke-linecap="square"
+                stroke-linejoin="bevel"
+                font-family="Noto Sans"
+                font-size="28"
+                font-size-adjust="0.372962"
+                font-stretch="normal"
+                font-weight="700"
+                letter-spacing="0"
+                text-anchor="middle"
+                word-spacing="0"
+            >
+                <tspan x="47.671875">Tiktok </tspan>
+                <tspan x="47.671875" dy="38.25">
+                    made{" "}
+                </tspan>
+                <tspan x="47.671875" dy="38.25">
+                    me{" "}
+                </tspan>
+                <tspan x="47.671875" dy="38.25">
+                    buy it
+                </tspan>
+            </text>
+            <path
+                transform="matrix(0.591049131505232 0 0 0.591049131505232 167.338219751845 67.5162802586804)"
+                fill="#01f1ed"
+                fill-rule="evenodd"
+                stroke="#ffffff"
+                stroke-width="3.6"
+                stroke-linecap="square"
+                stroke-linejoin="bevel"
+                d="M0 300.838L70.1494 265.378L53.7216 7.6557L29.4531 5.85596L9.12947 0"
+            />
+            <path
+                transform="matrix(0.591049131505232 0 0 0.591049131505232 166.824533734844 212.639469968346)"
+                fill="#ff6192"
+                fill-rule="evenodd"
+                stroke="#ffffff"
+                stroke-width="3.6"
+                stroke-linecap="square"
+                stroke-linejoin="bevel"
+                d="M0 54.9847L36.9602 0L70.5126 19.3756"
+            />
+            <ellipse
+                transform="matrix(0.591049124563687 0 0 0.591049124563687 99.8577982135385 80.1233272077463)"
+                rx="5.7150101812436"
+                ry="6.84000000000003"
+                cx="5.7150101812436"
+                cy="6.84000000000003"
+                fill="#ffffff"
+                fill-rule="evenodd"
+                stroke-opacity="0"
+                stroke="#000000"
+                stroke-width="0"
+                stroke-linecap="square"
+                stroke-linejoin="bevel"
+            />
+            <ellipse
+                transform="matrix(0.591049124563687 0 0 0.591049124563687 141.211145228589 77.8040502895675)"
+                rx="5.7150101812436"
+                ry="6.84000000000003"
+                cx="5.7150101812436"
+                cy="6.84000000000003"
+                fill="#ffffff"
+                fill-rule="evenodd"
+                stroke-opacity="0"
+                stroke="#000000"
+                stroke-width="0"
+                stroke-linecap="square"
+                stroke-linejoin="bevel"
+            />
+            <path
+                transform="matrix(0.591049131505232 0 0 0.591049131505232 103.230007409065 41.0097338864954)"
+                fill="none"
+                stroke="#ff004f"
+                stroke-width="7.2"
+                stroke-linecap="square"
+                stroke-linejoin="bevel"
+                d="M0 69.4871C0 60.0778 5.81979 24.1413 11.5746 18.3866C22.147 7.81415 63.7027 -7.96523 76.3746 4.70657C83.1491 11.4812 70.5018 57.6789 70.5018 67.8121"
+            />
+        </svg> 
+        {/* Blue wave */}
+        <svg id="shape0" width="100%" height="1474.56pt">
+            <path
+                transform="translate(-26.0070562941524, -6.47999962218574)"
+                fill="#02f1ee"
+                fillOpacity="0.854901960784314"
+                fillRule="evenodd"
+                stroke="none"
+                strokeWidth="0.72"
+                strokeLinecap="square"
+                strokeLinejoin="bevel"
+                d="M0.534202 0C-2.28098 9.31132 -3.26244 114.585 253.254 150.12C383.925 168.01 577.383 135.885 725.847 160.92C899.736 186.659 962.296 239.068 1108.61 208.44C1319.84 176.38 1506.74 14.1725 1526.57 7.56"
+            />
+        </svg>
+        {/* Red wave */}
+        <svg id="shape1" width="100%" height="1474.56pt">
+            <path
+                transform="matrix(0.999999969189725 0.000705676136402657 2.4492935228313e-16 0.999999969189725 -91.8658046810187 -82.6625549407509)"
+                fill="#ff004f"
+                fillOpacity="0.619607843137255"
+                fillRule="evenodd"
+                strokeOpacity="0"
+                stroke="#000000"
+                strokeWidth="0"
+                strokeLinecap="square"
+                strokeLinejoin="bevel"
+                d="M4.60187 0C2.45529 11.1089 -23.1978 120.698 74.5858 199.25C115.081 231.781 186.653 266.411 314.095 290.415C464.929 321.224 635.941 112.224 796.026 154.461C983.526 197.886 1023.82 401.343 1192.39 353.981C1322.22 323.148 1494.42 244.498 1582.27 157.146C1648.52 91.2597 1647.07 8.16633 1654.27 5.91126"
+            />
+        </svg>
+        {/* White wave */}
+        <svg id="shape2" width="100%" height="1474.56pt">
+            <path
+                transform="translate(-26.0182139414758, -7.55999955921669)"
+                fill="#ffffff"
+                fillRule="evenodd"
+                stroke="none"
+                strokeWidth="0.72"
+                strokeLinecap="square"
+                strokeLinejoin="bevel"
+                d="M0.556517 0C-2.25867 9.31132 -4.04725 115.665 252.469 151.2C321.301 161.312 458.478 155.09 466.309 153.54C487.612 146.195 617.647 58.9774 722.629 76.68C826.957 92.1227 910.118 203.298 931.61 207.261C961.324 215.592 1034.9 227.532 1109.81 208.98C1321.04 176.92 1506.76 14.1725 1526.6 7.56"
+            />
+        </svg>
+    </header>
+    )
+}
