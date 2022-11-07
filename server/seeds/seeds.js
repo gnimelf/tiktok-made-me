cd00b1bac4325c8782e0dc721f356fceb8407c8f
justin <fezzygo@gmail.com>
2022-11-06 20:15:19 -0500

    started working on the server/seeds/profileSeeds.json,  server/seeds/seeds.js)

---
+++ server/seeds/seeds.js
@@ -0,0 +1,9 @@
+// db
+const db = require('../config/connection');
+
+// Models
+const { Profile, Post } = require('../models');
+
+// seed data
+const profileSeds = require();
+const posts = require();
