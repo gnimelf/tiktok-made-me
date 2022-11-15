3cbc382de18b54a90f91a8fd2db1070bfe1e7b54
justin <fezzygo@gmail.com>
2022-11-14 23:45:14 -0500

    created logo192 and logo 512 sized ( client/src/components/PostList/index.js:5-31, client/src/App.js:45-71)

--- client/src/components/PostList/index.js
+++ client/src/components/PostList/index.js
@@ -2,40 +2,34 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
 const PostList = ({ posts }) => {
-    if (!posts.length) {
-        return <h3>No posts yet</h3>;
-    }
-    return (
-        <>
-            {posts.map((post) => (
-                <Card key={post._id} className="mx-2 my-2">
-                    <Card.Header className="text-center">
-                        <strong>{post.title}</strong>
-                    </Card.Header>
-                    <Card.Body className="d-flex justify-content-between align-items-center">
-                        <Card.Img
-                            className="product-image"
-                            variant="top"
-                            src={post.image}
-                        />
-                        <Card.Text className="align-middle">
-                            {post.description}
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer className="d-flex justify-content-around  align-items-center">
-                        <Button className="add-comment" variant="primary">
-                            Add comment
-                        </Button>
-                        <a href={post.url}>
-                            <Button className="buy-item" variant="info">
-                                Buy Item
-                            </Button>
-                        </a>
-                    </Card.Footer>
-                </Card>
-            ))}
-        </>
-    );
+  if (!posts.length) {
+    return <h3>No posts yet</h3>;
+  }
+  return (
+    <>
+      {posts.map((post) => (
+        <Card key={post._id} className="card my-2 mx-2" style={{width: "20rem"}}>
+          <img src={post.image} className="card-img-top" alt="product"/>
+          <div className="card-body">
+          <h5 class="card-title"><strong>{post.title}</strong></h5>
+            <p className="card-text">
+              {post.description}
+            </p>
+          </div>
+          <div className="card-footer d-flex justify-content-around  align-items-center">
+            <Button className="add-comment" variant="primary">
+              Add comment
+            </Button>
+            <a href={post.url}>
+              <Button className="buy-item" variant="info">
+                Buy Item
+              </Button>
+            </a>
+          </div>
+        </Card>
+      ))}
+    </>
+  );
 };
 
 export default PostList;
