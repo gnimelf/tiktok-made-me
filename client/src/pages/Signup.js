2a2e9640e3049c0350401df713c3db63621e08fc
justin <fezzygo@gmail.com>
2022-11-21 09:12:29 -0500

    added functions to handle input field change and form submission for signup form (client/src/pages/Signup:1,3-5,8-40,43,48,50,51,58,60,61,68,70,71,78,80,81,89, 91,92), added gql mutation for adding profile (client/src/utils/mutations.js:15-25)

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,31 +1,96 @@
-import React from "react";
-
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
+import { useMutation } from "@apollo/client";
+import { ADD_PROFILE } from "../utils/mutations";
+import Auth from "../utils/auth";
 
 export default function Signup(props) {
+    const [formState, setFormState] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        userName: "",
+        password: "",
+    });
+    const [addProfile] = useMutation(ADD_PROFILE);
+
+    // Handle input change
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        console.log(value);
+
+        setFormState({ ...formState, [name]: value });
+    };
+
+    // Handle form submit
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const { data } = await addProfile({ variables: formState });
+            const { token, userName } = data.addProfile.token;
+
+            console.log(userName);
+
+            Auth.login(token);
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
     return (
         <main>
-            <Form>
-            <Form.Group className="mb-3" controlId="formBasicFirst">
+            <Form onSubmit={handleFormSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicFirst">
                     <Form.Label>First Name</Form.Label>
-                    <Form.Control type="text" placeholder="john" />
+                    <Form.Control
+                        type="text"
+                        onChange={handleChange}
+                        placeholder="john"
+                        name="firstName"
+                        value={formState.firstName}
+                    />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicLast">
                     <Form.Label>Last Name</Form.Label>
-                    <Form.Control type="text" placeholder="doe" />
+                    <Form.Control
+                        type="text"
+                        onChange={handleChange}
+                        placeholder="doe"
+                        name="lastName"
+                        value={formState.lastName}
+                    />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" placeholder="john@example.com" />
+                    <Form.Control
+                        type="email"
+                        onChange={handleChange}
+                        placeholder="john@example.com"
+                        name="email"
+                        value={formState.email}
+                    />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicUsername">
                     <Form.Label>Username</Form.Label>
-                    <Form.Control type="text" placeholder="johndoe" />
+                    <Form.Control
+                        type="text"
+                        onChange={handleChange}
+                        placeholder="johndoe"
+                        name="userName"
+                        value={formState.userName}
+                    />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="password" />
+                    <Form.Control
+                        type="password"
+                        onChange={handleChange}
+                        placeholder="password"
+                        name="password"
+                        value={formState.password}
+                    />
                 </Form.Group>
                 <Button className="login-btn" type="submit">
                     Signup
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -12,3 +12,14 @@ mutation login($userName: String!, $password: String!) {
   }
   `;
 
+export const ADD_PROFILE = gql`
+mutation AddProfile($firstName: String!, $lastName: String!, $email: String!, $userName: String!, $password: String!) {
+  addProfile(firstName: $firstName, lastName: $lastName, email: $email, userName: $userName, password: $password) {
+    token
+    profile {
+      _id
+      userName
+    }
+  }
+}
+`;
