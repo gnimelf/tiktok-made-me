717cc769babc476246fa9e4230ff9ba3b12089a5
justin <fezzygo@gmail.com>
2022-11-20 21:03:52 -0500

    added mutations file to handle the client gql methods to communicated to backend gql (client/src/utils/mutations.js:1-13)

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,14 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+mutation login($userName: String!, $password: String!) {
+    login(userName: $userName, password: $password) {
+      token
+      profile {
+        _id
+        userName
+      }
+    }
+  }
+  `;
+
