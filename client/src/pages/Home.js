442d9dee1b4c7d076a2e4784e2fde327c3b18a81
justin <fezzygo@gmail.com>
2022-11-30 16:41:58 -0500

    formatted code stucture in document(client/src/pages/Home.js)

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,21 +1,23 @@
 import React from "react";
 import PostList from "../components/PostList";
-import Row from 'react-bootstrap/Row';
-import { useQuery } from '@apollo/client';
-import { QUERY_POSTS } from '../utils/queries'
+import Row from "react-bootstrap/Row";
+import { useQuery } from "@apollo/client";
+import { QUERY_POSTS } from "../utils/queries";
 
 const Home = () => {
-    const { loading, data } =  useQuery(QUERY_POSTS);
+    const { loading, data } = useQuery(QUERY_POSTS);
     const posts = data?.posts || [];
 
     return (
         <main>
-            <div >
-            <Row xl={3} xxl={4} className="justify-content-center g-0">
-                {loading ? (<div>loading...</div>) : (
-                    <PostList posts={posts}/>
-                )}
-            </Row>
+            <div>
+                <Row xl={3} xxl={4} className="justify-content-center g-0">
+                    {loading ? (
+                        <div>loading...</div>
+                    ) : (
+                        <PostList posts={posts} />
+                    )}
+                </Row>
             </div>
         </main>
     );
