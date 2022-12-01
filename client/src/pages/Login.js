b0d9b02b6406ca1809dca04b25dab36a0d7ddec5
justin <fezzygo@gmail.com>
2022-11-30 23:57:09 -0500

    fixed login page to show an error if password or username are incorrect (client/src/pages/Login.js:7, 9, 42-45, 70-75), added commponent to display all comments from a user if they are logged in (client/src/pages/MyComments.js:1-63)

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,18 +1,17 @@
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
-import { useMutation } from '@apollo/client';
+import { useMutation } from "@apollo/client";
 import { LOGIN_USER } from "../utils/mutations";
 import Auth from "../utils/auth";
 
-const LoginForm = (props) => {
+const LoginForm = () => {
     const [formState, setFormState] = useState({ userName: "", password: "" });
-    const [loginUser] = useMutation(LOGIN_USER);
+    const [loginUser, { error, data }] = useMutation(LOGIN_USER);
 
     // handle input changes
     const handleChange = (event) => {
         const { name, value } = event.target;
-
-        setFormState({...formState, [name]: value });
+        setFormState({ ...formState, [name]: value });
     };
 
     // handle form submit
@@ -39,8 +38,11 @@ const LoginForm = (props) => {
 
     return (
         <main>
-            <Form onSubmit={handleFormSubmit}>
-                <Form.Group className="mb-3" controlId="formBasicUsername">
+            {data ? (
+                <p>Success</p>
+            ) : (
+                <Form onSubmit={handleFormSubmit}>
+                    <Form.Group className="mb-3" controlId="formBasicUsername">
                         <Form.Label>Username</Form.Label>
                         <Form.Control
                             onChange={handleChange}
@@ -61,13 +63,16 @@ const LoginForm = (props) => {
                             placeholder="*********"
                         />
                     </Form.Group>
-                    <Button
-                        className="login-btn"
-                        type="submit"
-                    >
+                    <Button className="login-btn" type="submit">
                         Login
                     </Button>
                 </Form>
+            )}
+            {error && (
+                <div className="my-3 p-3 bg-danger text-white">
+                    {error.message}
+                </div>
+            )}
         </main>
     );
 };
